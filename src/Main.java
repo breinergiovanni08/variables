e907f468fd7ba7b2b4f860cb41ac28f0177041c1
breinergiovanni08 <110684736+breinergiovanni08@users.noreply.github.com>
2022-10-25 16:53:37 -0500

    firts commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+            Scanner Sc = new Scanner(System.in);
+            String nombre;
+            int nombre;
+            int dias;
+            int sueldo;
+            String nombre2;
+            int nombre2;
+            int dias2;
+            int sueldo2;
+            String nombre3;
+            int nombre3;
+            int dias3;
+            int sueldo3;
+            String nombre4;
+            int nombre4;
+            int dias4;
+            int sueldo4;
+            String nombre5;
+            int nombre5;
+            int dias5;
+            int sueldo5;
+            sueldo = 800000;
+            double horas;
+            System.out.println("bienvenido, porfavor digitar tu nombre");
+            nombre = Sc.nextline();
+            System.out.println("digitar su edad");
+            dias = Sc.nextInt();
+            System.out.println("digitar las horas de trabajo realizadas");
+            horas = Sc.nextByte();
+            System.out.println("su nombre es" +nombre+ "su edad es" +dias+ "sus horas realizadas son" +horas+);
+            System.out.println("su suedo es de" +sueldo+ "Gracias por ingresar la informacion requerida");
+
+
+            System.out.println("bienvenido, porfavor digitar tu nombre");
+            nombre2 = Sc.nextline();
+            System.out.println("digitar su edad");
+            dias2 = Sc.nextInt();
+            System.out.println("digitar las horas de trabajo realizadas");
+            horas2 = Sc.nextByte();
+            System.out.println("su nombre es" +nombre+ "su edad es" +dias+ "sus horas realizadas son" +horas+);
+            System.out.println("su suedo es de" +sueldo+ "Gracias por ingresar la informacion requerida");
+
+
+            System.out.println("bienvenido, porfavor digitar tu nombre");
+            nombre3 = Sc.nextline();
+            System.out.println("digitar su edad");
+            dias3 = Sc.nextInt();
+            System.out.println("digitar las horas de trabajo realizadas");
+            horas3 = Sc.nextByte();
+            System.out.println("su nombre es" +nombre+ "su edad es" +dias+ "sus horas realizadas son" +horas+);
+            System.out.println("su suedo es de" +sueldo+ "Gracias por ingresar la informacion requerida");
+
+
+            System.out.println("bienvenido, porfavor digitar tu nombre");
+            nombre4 = Sc.nextline();
+            System.out.println("digitar su edad");
+            dias4 = Sc.nextInt();
+            System.out.println("digitar las horas de trabajo realizadas");
+            horas4 = Sc.nextByte();
+            System.out.println("su nombre es" +nombre+ "su edad es" +dias+ "sus horas realizadas son" +horas+);
+            System.out.println("su suedo es de" +sueldo+ "Gracias por ingresar la informacion requerida");
+
+
+            System.out.println("bienvenido, porfavor digitar tu nombre");
+            nombre5 = Sc.nextline();
+            System.out.println("digitar su edad");
+            dias5 = Sc.nextInt();
+            System.out.println("digitar las horas de trabajo realizadas");
+            horas5 = Sc.nextByte();
+            System.out.println("su nombre es" +nombre+ "su edad es" +dias+ "sus horas realizadas son" +horas+);
+            System.out.println("su suedo es de" +sueldo+ "Gracias por ingresar la informacion requerida");
+
+
+
+
+        }
